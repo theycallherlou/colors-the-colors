4be4e114dc0ffe61bc8dcf5e293a10b1442dad73
Adam Robson <adamrayrobson@gmail.com>
2024-08-14 16:40:52 -0700

    add color array
    Signed-off-by: Adam Robson <adamrayrobson@gmail.com>

---
+++ src/lib/color.js
@@ -0,0 +1,13 @@
+export const colors = [
+  { name: 'olive', rgb: ['37', '38', '33'] },
+  { name: 'munsell', rgb: ['34', '8b', 'a8'] },
+  { name: 'bittersweet', rgb: ['eb', '60', '56'] },
+  { name: 'naples', rgb: ['fe', 'd8', '67'] },
+  { name: 'alice', rgb: ['dc', 'e1', 'e9'] },
+  { name: 'baby', rgb: ['f1', 'de', 'de'] },
+  { name: 'cobalt', rgb: ['3d', '47', '54'] },
+  { name: 'gray', rgb: ['eb', '60', '56'] },
+  { name: 'dark gray', rgb: ['4d', '4d', '4d'] },
+  { name: 'jet', rgb: ['33', '33', '33'] },
+  { name: 'black', rgb: ['1a', '1a', '1a'] },
+];
